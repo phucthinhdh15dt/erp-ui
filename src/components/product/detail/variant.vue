11a71177ba0666fb22a1fbd829294fbcb80e33f2
mxvi-bk <mxvi.bk@gmail.com>
2022-01-28 09:13:53 +0700

    add dayjs

--- src/components/product/detail/variant.vue
+++ src/components/product/detail/variant.vue
@@ -21,12 +21,10 @@
 
 <script setup>
 import { computed, inject, toRefs } from 'vue';
-import { useStore } from 'vuex';
-import { Card, Table, Form, Select, Button } from 'ant-design-vue';
+import { Card, Table } from 'ant-design-vue';
 import StatusTags from '../materials/statusTags.vue';
 
 const { Column } = Table;
-const { Item: FormItem } = Form;
 
 const props = defineProps({
     attributes: {
@@ -37,8 +35,6 @@ const props = defineProps({
 const { attributes } = toRefs(props);
 const modelRef = inject('modelRef');
 
-const store = useStore();
-
 const columns = computed(() => [
     { title: 'Product code', dataIndex: 'productCode', key: 'productCode' },
     ...attributes.value.map(_ => ({
@@ -47,24 +43,5 @@ const columns = computed(() => [
         key: _.code,
     })),
 ]);
-
-const defaultData = computed(() =>
-    columns.value.reduce((acc, cur) => {
-        acc[cur.key] = null;
-
-        return acc;
-    }, {})
-);
-const add = () => {
-    store.commit('product/addVariant', defaultData.value);
-};
-
-const remove = index => {
-    store.commit('product/removeVariant', { index });
-};
-
-const onChange = (field, index, value) => {
-    store.commit('product/setVariantData', { field, index, value });
-};
 </script>
 
